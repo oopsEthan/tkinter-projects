ad16a46bd2e10c1f2f1a3d1c391a237a098e6f4d
Ethan <ethan_here@proton.me>
2024-10-12 18:21:45 -0500

    Init repo

---
+++ mi_to_km_converter.py
@@ -0,0 +1,41 @@
+# from pandas import *
+from tkinter import *
+
+class Main_Window(Tk):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.title("Miles to Kilometers Converter")
+        self.config(padx=20, pady=10)
+
+        self.setup_layout()
+
+    def setup_layout(self):
+        self.mile_input = Entry(width=10)
+        self.mile_input.grid(column=2, row=1)
+
+        self.result_text = Label(text="0")
+        self.result_text.grid(column=2, row=2)
+
+        km_text = Label(text="Km")
+        km_text.grid(column=3, row=2)
+
+        mile_text = Label(text="Mi")
+        mile_text.grid(column=3, row=1)
+
+        equal_text = Label(text="is equal to")
+        equal_text.grid(column=1, row=2)
+
+        convert_button = Button(text="Convert", command=self.convert_miles)
+        convert_button.grid(column=2, row=3)
+
+    def convert_miles(self):
+        try:
+            miles = float(self.mile_input.get())
+            kilometers = miles * 1.60934
+            self.result_text.config(text=f"{kilometers:.2f}")
+        except ValueError:
+            self.result_text.config(text="INVALID!")
+
+window = Main_Window()
+window.mainloop()
---
+++ readme.txt
@@ -0,0 +1,4 @@
+List of projects (as added):
+
+1 - mi_to_km_converter.py
+    - Converts miles to kilometers using a tkinter GUI
