6f9dd448287537c705b2edecc483fd5229515980
oopsEthan <ethan_here@proton.me>
2024-10-24 21:45:04 -0500

    minor change

--- pomodoro.py
+++ pomodoro.py
@@ -14,9 +14,9 @@ if timer:
     start_button = Button(text="Start", command=timer.start_timer)
     reset_button = Button(text="Reset", command=timer.reset_timer)
 
-timer.grid(row = 2, column = 3)
+    timer.grid(row = 2, column = 3)
 
-start_button.grid(row = 4, column = 2)
-reset_button.grid(row = 4, column = 4)
+    start_button.grid(row = 4, column = 2)
+    reset_button.grid(row = 4, column = 4)
 
 window.mainloop()
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
-List of projects (as added):
+List of projects:
 
 1 - mi_to_km_converter.py
     - Converts miles to kilometers using a tkinter GUI
 
 2 - pomodoro.py
-    - Pomodoro technique timer
+    - Pomodoro technique timer using a tkinter GUI
