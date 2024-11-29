14f4e440bcfd8f5ff6123347fba8d1964e5b51d9
oopsEthan <ethan_here@proton.me>
2024-11-29 17:30:51 -0600

    Improvements

--- pomodoro.py
+++ pomodoro.py
@@ -4,19 +4,56 @@ from pomodoro_obj import *
 # Constants
 PAD_X = 200
 PAD_Y = 100
-YELLOW = "#f7f5dd"
+BLACK = "#000"
+PINK = "#E2979C"
+RED = "#E7305B"
+GREEN = "#9BDEAC"
+YELLOW = "#F7F5DD"
+FONT_NAME = "Courier"
+
+# Grid Constants
+CENTER = 3
+BUTTON_ROW = 4
 
 window = Tk()
 window.config(padx=PAD_X, pady=PAD_Y, bg=YELLOW)
 
+display_for_timer_state = Label(text="OFF", font=(FONT_NAME, 32, "bold"), background=YELLOW, width=11)
+rotation_count = Label(text="", font=(FONT_NAME, 16, "bold"), background=YELLOW, fg=GREEN)
+
 timer = Timer()
 
-if timer:
-    start_button = Button(text="Start", command=timer.start_timer)
-    reset_button = Button(text="Reset", command=timer.reset_timer)
+def update_UI():
+    # Update display to show correct timer_state
+    display_for_timer_state.config(text=f"{timer.timer_state}", fg=timer.timer_state_color)
 
-    timer.grid(row = 2, column = 3)
-    start_button.grid(row = 4, column = 2)
-    reset_button.grid(row = 4, column = 4)
+    # Update rotation_count by rotations
+    rotations = 0
+    rotation_display = ""
+    while rotations < timer.timer_rotations:
+        rotation_display += "✓"
+        rotations += 1
+
+    rotation_count.config(text=f"{rotation_display}")
+    
+    window.after(1000, update_UI)
+
+def start():
+    timer.start_timer()
+    update_UI()
+
+def reset():
+    timer.reset_timer()
+    update_UI()
+
+if timer:
+    start_button = Button(text="Start", command=start)
+    reset_button = Button(text="Reset", command=reset)
+
+    timer.grid(row=2, column=CENTER)
+    start_button.grid(row=BUTTON_ROW, column=2)
+    reset_button.grid(row=BUTTON_ROW, column=4)
+    display_for_timer_state.grid(row=0, column=CENTER)
+    rotation_count.grid(row=5, column=CENTER)
 
 window.mainloop()
--- pomodoro_obj.py
+++ pomodoro_obj.py
@@ -11,7 +11,14 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 15
 
+# Test minutes
+# WORK_MIN = 1
+# SHORT_BREAK_MIN = 1
+# LONG_BREAK_MIN = 1
+
 # Other constants
+# ✓
+BLACK = "#000"
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -23,6 +30,7 @@ class Timer(Canvas):
         super().__init__()
         
         self.timer_state = OFF
+        self.timer_state_color = BLACK
         self.timer_rotations = 0
 
         self.set_timer_visuals()
@@ -34,12 +42,14 @@ class Timer(Canvas):
 
         self.create_image(tomato_width/2, tomato_height/2, image=self.tomato_png)
 
-        self.timer_state_label = self.create_text(tomato_width/2, tomato_height/1.4, text=f"{self.timer_state}", font=(FONT_NAME, 16, "bold"))
-        self.timer_text = self.create_text(tomato_width/2, tomato_height/1.75, text="00:00", font=(FONT_NAME, 24, "bold"))
+        # self.timer_state_label = self.create_text(tomato_width/2, tomato_height/1.4, text=f"{self.timer_state}", font=(FONT_NAME, 16, "bold"))
+        self.timer_text = self.create_text(tomato_width/2, tomato_height/1.75, text="00:00", font=(FONT_NAME, 24, "bold"), fill="#ffffff")
     
     def start_timer(self) -> None:
-        self.timer_state = WORK
-        self.set_timer(WORK_MIN)
+        if self.timer_state == OFF:
+            self.timer_state = WORK
+            self.timer_state_color = GREEN
+            self.set_timer(WORK_MIN)
 
     def set_timer(self, minutes) -> None:
         self.time_left = minutes * 60
@@ -49,41 +59,46 @@ class Timer(Canvas):
     def countdown(self) -> None:
         if self.timer_state != OFF:
             if self.time_left == 0:
-                self.timer_state = self.update_state()
+                self.update_state()
             elif self.time_left > 0:
                 self.time_left -= 1
                 self.format_timer()
                 self.after(1000, self.countdown)
     
-    def update_state(self) -> str:
+    def update_state(self) -> None:
         # Set to short break if work is over and still under 4 rotations
         if self.timer_state == WORK and self.timer_rotations != 4:
             self.timer_rotations += 1
+            self.timer_state = SHORT_BREAK
+            self.timer_state_color = PINK
             self.set_timer(SHORT_BREAK_MIN)
-            return SHORT_BREAK
         
         # Set to work if short break is over and still under 4 rotations
         elif self.timer_state == SHORT_BREAK:
+            self.timer_state = WORK
+            self.timer_state_color = GREEN
             self.set_timer(WORK_MIN)
-            return WORK
         
         # Set to work if long break is over and reset rotations
         elif self.timer_state == LONG_BREAK:
             self.timer_rotations = 0
+            self.timer_state = WORK
+            self.timer_state_color = GREEN
             self.set_timer(WORK_MIN)
-            return WORK
         
         # Set to long break if 4 rotations is over
         elif self.timer_rotations == 4:
+            self.timer_state = LONG_BREAK
+            self.timer_state_color = RED
             self.set_timer(LONG_BREAK_MIN)
-            return LONG_BREAK
         
         # If something goes wrong, reset completely
-        self.reset_timer()
-        return OFF
+        else:
+            self.reset_timer()
 
     def reset_timer(self) -> None:
         self.timer_state = OFF
+        self.timer_state_color = BLACK
         self.timer_rotations = 0
         self.time_left = 0
         self.format_timer()
@@ -91,5 +106,5 @@ class Timer(Canvas):
     def format_timer(self) -> None:
         minutes_left = self.time_left // 60
         seconds_left = self.time_left % 60
-        self.itemconfig(self.timer_state_label, text=f"{self.timer_state}")
+        # self.itemconfig(self.timer_state_label, text=f"{self.timer_state}")
         self.itemconfig(self.timer_text, text=f"{minutes_left:02}:{seconds_left:02}")
