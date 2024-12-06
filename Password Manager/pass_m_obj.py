f2a969f0460ee5edab4529c2ae020ee042950355
oopsEthan <ethan_here@proton.me>
2024-12-06 01:39:05 -0600

    Added exception handling and .JSON

--- Password Manager/pass_m_obj.py
+++ Password Manager/pass_m_obj.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice
 from string import *
+import json
 
 # Constants
 LABEL_COLUMN = 0
@@ -19,27 +20,27 @@ class InputForm():
     
     def generate_labels(self) -> None:
         self.website_label = Label(text="Website:")
-        self.website_label.grid(row=WEBSITE_ROW, column=LABEL_COLUMN)
+        self.website_label.grid(row=WEBSITE_ROW, column=LABEL_COLUMN, sticky="W")
         self.email_label = Label(text="Email/Username:")
-        self.email_label.grid(row=EMAIL_ROW, column=LABEL_COLUMN)
+        self.email_label.grid(row=EMAIL_ROW, column=LABEL_COLUMN, sticky="W")
         self.password_label = Label(text="Password:")
-        self.password_label.grid(row=PASSWORD_ROW, column=LABEL_COLUMN)
+        self.password_label.grid(row=PASSWORD_ROW, column=LABEL_COLUMN, sticky="W")
 
     def generate_input_fields(self) -> None:
-        self.website_input = Entry(width=35)
-        self.website_input.grid(row=WEBSITE_ROW, column=ENTRY_COLUMN, columnspan=2)
-        self.email_input = Entry(width=35)
-        self.email_input.grid(row=EMAIL_ROW, column=ENTRY_COLUMN, columnspan=2)
-        self.password_input = Entry(width=20)
-        self.password_input.grid(row=PASSWORD_ROW, column=ENTRY_COLUMN)
+        self.website_input = Entry()
+        self.website_input.grid(row=WEBSITE_ROW, column=ENTRY_COLUMN, columnspan=2, sticky="EW")
+        self.email_input = Entry()
+        self.email_input.grid(row=EMAIL_ROW, column=ENTRY_COLUMN, columnspan=2, sticky="EW")
+        self.password_input = Entry()
+        self.password_input.grid(row=PASSWORD_ROW, column=ENTRY_COLUMN, sticky="EW")
 
         self.get_entries()
 
     def generate_buttons(self) -> None:
-        self.generate_password_button = Button(text="Generate Password", width=11, command=self.generate_password)
-        self.generate_password_button.grid(row=PASSWORD_ROW, column=2)
-        self.save_input_form_button = Button(text="Add", width=32, command=self.save_entry)
-        self.save_input_form_button.grid(row=4, column=ENTRY_COLUMN, columnspan=2)
+        self.generate_password_button = Button(text="Generate Password", command=self.generate_password)
+        self.generate_password_button.grid(row=PASSWORD_ROW, column=2, sticky="EW")
+        self.save_input_form_button = Button(text="Add", command=self.save_entry)
+        self.save_input_form_button.grid(row=4, column=ENTRY_COLUMN, columnspan=2, sticky="EW")
 
     def validate_entry(self) -> bool:
         self.get_entries()
@@ -48,14 +49,28 @@ class InputForm():
             messagebox.showerror(title="ERROR: Invalid Entries", message="One or more of your entry fields is blank.")
             return False
 
+        self.data_package = {
+            self.website: {
+                "email": self.email,
+                "password": self.password
+            }
+        }
+
         return messagebox.askquestion(title="Please Confirm", message=f"Are you sure you want to save this information?\n\nEmail/Username: {self.email}\nPassword: {self.password}")
 
     def save_entry(self) -> None:
         if self.validate_entry():
-            save_file = open("logins.txt", "a")
-            save_string = f"{self.website} | {self.email} | {self.password}\n"
-            save_file.write(save_string)
-            save_file.close()
+            try:
+                with open("logins.json", "r") as save_file:
+                    data = json.load(save_file)
+                    data.update(self.data_package)
+                
+                with open("logins.json", "w") as save_file:
+                    json.dump(data, save_file, indent=4)
+                    
+            except FileNotFoundError:
+                with open("logins.json", "w") as save_file:
+                    json.dump(self.data_package, save_file, indent=4)
                 
             self.reset_entry()
     
@@ -63,6 +78,8 @@ class InputForm():
         self.website_input.delete(0, len(self.website_input.get()))
         self.email_input.delete(0, len(self.email_input.get()))
         self.password_input.delete(0, len(self.password_input.get()))
+        self.data_package = {}
+
         self.get_entries()
     
     def get_entries(self) -> None:
@@ -87,7 +104,7 @@ class InputForm():
 class Logo(Canvas):
     def __init__(self) ->  None:
         super().__init__()
-        self.logo_img = PhotoImage(file="logo.png")
+        self.logo_img = PhotoImage(file="Password Manager/logo.png")
         logo_width, logo_height = self.logo_img.width(), self.logo_img.height()
         self.config(width=logo_width, height=logo_height, highlightthickness=0)
 
